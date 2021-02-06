3df2e426ed2e961d0bd851ca9c87954e083f32f8
Iván Serrano <contacto.ivanserrano@gmail.com>
2021-02-06 13:31:52 -0600

    Se crea arcvhiso sh de prueba

---
+++ prueba.sh
@@ -0,0 +1,34 @@
+!/bin/bash
+ls -lh
+pwd
+sudo apt-get upgrade 
+sudo apt-get --fix-broken install -y
+sudo apt-get clean
+sudo apt-get autoclean 
+sudo apt-get autoremove 
+sudo dpkg --configure -a asumatsapawt
+sudo apt-get update
+sudo apt-get install samba -y
+ssudo apt install default-jre
+sudo add-apt-repository ppa:webupd8team/ java 
+sudo apt install default-jdk
+java -version
+sudo apt update
+sudo apt install oracle-java8-installer -y
+sudo apt-get remove --purge libreoffice*
+sudo apt-get clean
+sudo apt-get autoremove
+wget https://download.anydesk.com/linux/anydesk_2.9.5-1_amd64.deb
+sudo dpkg -i anydesk_2.9.5-1_amd64.deb
+sudo apt install –f
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C5E224500C1289C0
+sudo nano /etc/apt/sources.list.d/teamviewer.list
+deb http://linux.teamviewer.com/deb/ stable main
+sudo apt update
+sudo apt install -y teamviewer
+sudo apt-get install -f -y
+sudo dpkg -i teamviewer_14.2.2558_amd64.deb
+sh libra-linux-x64-4_2_2-nojre.sh
+sudo apt-get upgrade
+sudo mv hosts /etc/ 
+pwd
